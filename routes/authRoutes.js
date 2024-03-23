f76d56273d2313f1f121ac5d82991b772623af46
satyansh <satyansh.shukla.28@gmail.com>
2024-03-23 20:11:13 +0530

    added checks

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,12 @@
 const express = require("express");
 
 const authController = require("./../controllers/authController");
-//const {checkAdmin,checkLogin}=require("./../middleware/authMiddleware")
+const {checkAdmin,checkLogin}=require("./../middleware/authMiddleware")
 const router = express.Router();
 
 
 router.post("/login", authController.login);
-router.post("/register",authController.register);
+router.post("/register",checkLogin,checkAdmin,authController.register);
 // router.get("/logout",checkLogin,authController.logout)
 // router.get("/resetPassword",checkLogin,authController.resetPassword);
 
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
+const express = require("express");
 require("dotenv").config();
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 8000;
@@ -9,7 +9,7 @@ const MONGODB_URI = process.env.MONGODB_URI;
 mongoose
   .connect(MONGODB_URI)
   .then(() => {
-    console.log("Connected to MongoDB!");    
+    console.log("Connected to MongoDB!");
   })
   .catch((err) => {
     console.log(err);
@@ -17,41 +17,48 @@ mongoose
     process.exit(1);
   });
 
-
-const authRoutes=require("./routes/authRoutes")
+const authRoutes = require("./routes/authRoutes");
+const { checkLogin } = require("./middleware/authMiddleware");
 const app = express();
 
 app.use(cookieParser());
 
 app.use(express.json({ limit: "1000kb" }));
-const cors=(req,res,next)=>{
+const cors = (req, res, next) => {
   //res.header("Access-Control-Allow-Origin", process.env.FRONTEND);
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,PATCH,DELETE");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.header("Access-Control-Allow-Credentials",true);
-  res.header("Access-Control-Expose-Headers","set-cookie");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT,PATCH,DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Expose-Headers", "set-cookie");
   next();
-}
+};
 
 app.use(cors);
 
-app.options("*",(req,res)=>{res.status(200).send()});
+app.options("*", (req, res) => {
+  res.status(200).send();
+});
 
 //routes
-app.use("/api/auth",authRoutes)
+app.use("/api/auth", authRoutes);
 
 //global-catch
-app.use((err,req,res,next)=>{  
-    if(err)
-    {
-        console.log(err)
-        res.status(400).send("Invalid Request.");
-    }
-})
+app.use(async (err, req, res, next) => {
+  console.log(err);
+  res.status(400).send("Invalid Request.");
+});
 
-
+process.on("uncaughtException", function (error) {
+  console.log(error);
+});
 
 app.listen(PORT, () => {
-    console.log(`listening to PORT ${PORT}`)
-})
+  console.log(`listening to PORT ${PORT}`);
+});
