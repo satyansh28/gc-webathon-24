e302ebe3cc0fc6d83da6a4acbeb98960f9f5d12b
Rishvic Pushpakaran <rishvic@gmail.com>
2024-03-25 14:48:05 +0530

    Added course-wise grading

--- gc-24-backend/routes/academicsRoutes.js
+++ gc-24-backend/routes/academicsRoutes.js
@@ -4,6 +4,7 @@ const {
   getCourse,
   getCourseStudents,
   addAttendance,
+  gradeCourseStudents,
 } = require("../controllers/academics/courseController");
 const router = express.Router();
 
@@ -11,5 +12,6 @@ router.get("/courses", getCourses);
 router.get("/courses/:id", getCourse);
 router.get("/courses/:id/students", getCourseStudents);
 router.post("/courses/:id/attendance", addAttendance);
+router.post("/courses/:id/grade", gradeCourseStudents);
 
 module.exports = router;
